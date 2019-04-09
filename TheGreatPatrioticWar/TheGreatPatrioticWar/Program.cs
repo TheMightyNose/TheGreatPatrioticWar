523e0782e82422009e602b38921ebf0ab859f4fa
TheMightyNose <hiddetelink@live.nl>
2019-04-09 15:43:18 +0200

    Added DLLs

--- TheGreatPatrioticWar/TheGreatPatrioticWar/Program.cs
+++ TheGreatPatrioticWar/TheGreatPatrioticWar/Program.cs
@@ -1,10 +1,13 @@
+using SFML.Window;
+
 namespace TheGreatPatrioticWar
 {
   static class Program
   {
     static void Main()
     {
-
+      Window window = new Window(new VideoMode(300, 300), "test");
+      for (; ; );
     }
   }
 }
